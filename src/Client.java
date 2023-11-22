97ffbdf26ba0c005c47dadbe2ecf70cd4cd9fb97
havanchau <chauhvc2.dy@gmail.com>
2023-11-22 15:39:38 +0700

    add update

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,8 @@
-
-// import java.io.BufferedOutputStream;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.nio.file.*;
 import java.io.FileInputStream;
-// import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -51,23 +48,14 @@ public class Client implements Runnable {
             // input peerId
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            // System.out.print(ANSI_BLUE + "Enter the peer ID: " + ANSI_RESET);
             InetAddress ip = InetAddress.getLocalHost();
             System.out.println(ANSI_YELLOW + "The IP address of server : " + ip.getHostAddress() + ANSI_RESET);
             peerID = ip.getHostAddress();
-            // while (!) {
-            // System.out.println(ANSI_YELLOW + "Client exists, please choose another
-            // peerID" + ANSI_RESET);
-            // System.out.print(ANSI_BLUE + "Enter the peer ID: " + ANSI_RESET);
-            // peerID = br.readLine();
-            // }
             hello.addClient(peerID);
 
             while (true) {
                 System.out.println(ANSI_BLUE + ">> Enter a command ('help' for the list of commands)" + ANSI_RESET);
                 String inputLine = br.readLine();
-                // inputLine = inputLine.replace("\uFFFC", "");
-                // System.out.println("Command: " + inputLine.length());
                 if (inputLine.equals("exit")) {
                     hello.removeClient(peerID);
                     break;
@@ -115,9 +103,24 @@ public class Client implements Runnable {
         try {
             FileSharingInterface hello = (FileSharingInterface) Naming.lookup("rmi://" + ipServer + "/Hello");
             try {
-                // register file in the directoryName with the remote object (server side).
-                // This method is done by the server, so thread of server will log the result of
-                hello.registerFiles(peerID, fileName, portno, directoryName);
+                File directoryList = new File(directoryName);
+                int counter = 0;
+                String[] store = directoryList.list();
+                boolean isAlive = false;
+                System.out.println(directoryList);
+                while (counter < store.length) {
+                    File currentFile = new File(store[counter]);
+                    if (fileName.equalsIgnoreCase(currentFile.getName())) {
+                        isAlive = true;
+                        break;
+                    }
+                    counter++;
+                }
+                if (isAlive) {
+                    hello.registerFiles(peerID, fileName, portno, directoryName);
+                } else {
+                    System.out.println("Can not find file to publish!.");
+                }
             } catch (RemoteException ex) {
                 Logger.getLogger(FileSharingClient.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -137,14 +140,30 @@ public class Client implements Runnable {
 
     public void fetchFile(String fileTobeSearched) {
         System.out.println(fileTobeSearched);
-        try {
-            FileSharingInterface hello = (FileSharingInterface) Naming.lookup("rmi://" + ipServer + "/Hello");
+        File directoryList = new File(this.directoryName);
+        int counter = 0;
+        String[] store = directoryList.list();
+        boolean isAlive = false;
+        while (counter < store.length) {
+            File currentFile = new File(store[counter]);
+            if (fileTobeSearched.equalsIgnoreCase(currentFile.getName())) {
+                isAlive = true;
+                break;
+            }
+            counter++;
+        }
+        if (isAlive) {
+            System.out.println("This file already exists in the directory!");
+        } else {
+            try {
+                FileSharingInterface hello = (FileSharingInterface) Naming.lookup("rmi://172.29.16.1/Hello");
 
-            ArrayList<FileDetails> FilesName = hello.searchFile(fileTobeSearched);
+                ArrayList<FileDetails> FilesName = hello.searchFile(fileTobeSearched);
 
-            downloadFile(FilesName);
-        } catch (Exception e) {
-            System.out.println(ANSI_RED + "HelloClient exception: " + e + ANSI_RESET);
+                downloadFile(FilesName);
+            } catch (Exception e) {
+                System.out.println(ANSI_RED + "HelloClient exception: " + e + ANSI_RESET);
+            }
         }
     }
 
@@ -175,15 +194,13 @@ public class Client implements Runnable {
             FileSharingInterface hello = (FileSharingInterface) Naming.lookup("rmi://" + ipServer + "/Hello");
             if (file.delete()) {
                 System.out.println(ANSI_BLUE + "File deleted successfully" + ANSI_RESET);
-    
-    
+
                 hello.deleteFile(fileName);
-            
+
             } else {
                 System.out.println(ANSI_RED + "Failed to delete the file" + ANSI_RESET);
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Operation Failed");
         }
     }
@@ -210,48 +227,7 @@ public class Client implements Runnable {
         out.write(fileContent);
         out.flush();
         out.close();
-        // byte[] fileContent = null;
-        // try {
-        // File file = new File(sourceDir + "\\" + FilesName.get(0).FileName);
-        // byte buffer[] = new byte[(int) file.length()];
-        // BufferedInputStream input = new BufferedInputStream(new
-        // FileInputStream(file));
-        // input.read(buffer, 0, buffer.length);
-        // input.close();
-        // fileContent = buffer;
-        // } catch (Exception e) {
-        // System.out.println("FileImpl: " + e.getMessage());
-        // e.printStackTrace();
-        // }
-        // peerServer.downloadFile(FilesName.get(0).FileName, fileContent);
-        // String source = sourceDir + "\\" + FilesName.get(0).FileName;
-        // // directory where file will be copied
-        // String target = directoryName;
 
-        // InputStream is = null;
-        // OutputStream os = null;
-        // try {
-        // File srcFile = new File(source);
-        // File destFile = new File(target);
-        // if (!destFile.exists()) {
-        // destFile.createNewFile();
-        // }
-        // is = new FileInputStream(srcFile);
-
-        // os = new FileOutputStream(target + "\\" + srcFile.getName());
-        // byte[] buffer = new byte[1024];
-        // int length;
-
-        // while ((length = is.read(buffer)) > 0) {
-        // os.write(buffer, 0, length);
-        // }
-        // System.out.println(ANSI_BLUE + "Download successfully!." + ANSI_RESET);
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // } finally {
-        // is.close();
-        // os.close();
-        // }
     }
 
     public static void main(String[] args) throws IOException {
@@ -269,5 +245,6 @@ public class Client implements Runnable {
             e.printStackTrace();
         }
         new Client(portno, args[0]).run();
+
     }
 }
--- src/FileImpl.java
+++ src/FileImpl.java
@@ -1,7 +1,3 @@
-
-// import java.io.BufferedInputStream;
-// import java.io.File;
-// import java.io.FileInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
